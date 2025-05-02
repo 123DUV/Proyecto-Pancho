cf1ea4e7d7d1e9d3bdc57d42371f843cc7fb1879
123DUV <mendezban416@gmail.com>
2025-05-02 14:16:22 -0500

    subir a infinity

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 //ruta principal
-$BASE_URL= 'http://localhost/app_duv/';
-
+// $BASE_URL= 'http://localhost/app_duv/';
+$BASE_URL= 'http://controlcoser.kesug.com/';
 //BD datos
 $DB_HOST = 'sql306.infinityfree.com';
 $DB_USERNAME = 'if0_38888091';
