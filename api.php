221a6c01678313fa4f315e72ec6183702d316697
123DUV <mendezban416@gmail.com>
2025-05-02 16:31:07 -0500

    subir a infinity

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 use function PHPSTORM_META\type;
-header("Access-Control-Allow-Origin: http://controlcoser.kesug.com/");
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
