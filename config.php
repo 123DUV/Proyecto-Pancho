d8106c9b2be8366dbd4fd2aae485738d5878b38a
123DUV <mendezban416@gmail.com>
2025-08-12 18:22:04 -0500

    name in blog

--- config.php
+++ config.php
@@ -1,31 +1,31 @@
 <?php
 //DATOS LOCAL
-$BASE_URL= 'http://localhost/app_duv/';
+// $BASE_URL= 'http://localhost/app_duv/';
 
-$DB_HOST = 'localhost';
-$DB_USERNAME = 'root';
-$DB_PASS = '';
-$DB_NAME = 'app-duv';
-$DB_F_TABLE = 'datos';
-
-
-$RUTA_PAGES = 'http://localhost/app_duv/pages/';
+// $DB_HOST = 'localhost';
+// $DB_USERNAME = 'root';
+// $DB_PASS = '';
+// $DB_NAME = 'app-duv';
+// $DB_F_TABLE = 'datos';
 
 
-$RUTA_API = 'app_duv/';
+// $RUTA_PAGES = 'http://localhost/app_duv/pages/';
+
+
+// $RUTA_API = 'app_duv/';
 
 //------------------------------------------------------------------------>
 
 //DATOS INFINITY
-        // $BASE_URL= 'https://controlcoser.kesug.com/';
+        $BASE_URL= 'https://controlcoser.kesug.com/';
 
-        // $DB_HOST = 'sql306.infinityfree.com';
-        // $DB_USERNAME = 'if0_38888091';
-        // $DB_PASS = 'du4van1234';
-        // $DB_NAME = 'if0_38888091_app_duv';
-        // $DB_F_TABLE = 'datos';
+        $DB_HOST = 'sql306.infinityfree.com';
+        $DB_USERNAME = 'if0_38888091';
+        $DB_PASS = 'du4van1234';
+        $DB_NAME = 'if0_38888091_app_duv';
+        $DB_F_TABLE = 'datos';
 
-        // $RUTA_PAGES = 'https://controlcoser.kesug.com/pages/';
+        $RUTA_PAGES = 'https://controlcoser.kesug.com/pages/';
 
-        // $RUTA_API = '';
+        $RUTA_API = '';
 ?>
